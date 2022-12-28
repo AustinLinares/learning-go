0d3cc2077606af8e209cfc64f1ae97d63649bc6a
AustinLinares <linaresaustin@hotmail.com>
2022-12-27 19:48:54 -0500

    slice copy

--- fundamentals/src/main.go
+++ fundamentals/src/main.go
@@ -293,6 +293,16 @@ func sliceCut() {
 	fmt.Println(razasDePerros)
 }
 
+func copySlice() {
+	figuras := []string{"circulo", "cuadrado", "triangulo", "rombo", "trapecio", "heptagono"}
+	var figuras2 = make([]string, len(figuras))
+	fmt.Println(figuras)
+	copy(figuras2, figuras)
+	figuras = append(figuras[:1], figuras[2:]...)
+	fmt.Println(figuras2)
+	fmt.Println(figuras)
+}
+
 func main() {
-	sliceCut()
+	copySlice()
 }
